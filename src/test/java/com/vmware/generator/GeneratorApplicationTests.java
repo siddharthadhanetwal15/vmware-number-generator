00db8986ca2875ec8e4931c38ec167578e1c3f57
Siddhartha Dhanetwal <siddharthadhanetwal@Siddharthas-MacBook-Pro.local>
2020-06-21 20:31:34 +0530

    add test cases

--- src/test/java/com/vmware/generator/GeneratorApplicationTests.java
+++ src/test/java/com/vmware/generator/GeneratorApplicationTests.java
@@ -1,21 +1,51 @@
 package com.vmware.generator;
 
-import com.vmware.generator.service.TaskSvc;
+import com.vmware.generator.repository.TaskRepository;
+import com.vmware.generator.service.impl.TaskSvcImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
 class GeneratorApplicationTests {
-	@Autowired
-	TaskSvc taskSvc;
+	@InjectMocks
+	private TaskSvcImpl taskSvc;
+
+	@Mock
+	private TaskRepository taskRepository;
+
+	/**
+	 * below are the test cases for testing this app. We need to mock the object and then assert with the answer
+	 */
 
 	@Test
-	void contextLoads() {
+	void performTaskTest(){
+
 	}
 
 	@Test
-	void performTask(){
+	void performTaskInvalidBodyTest(){
+
+	}
+
+	@Test
+	void getTaskStatusTest(){
+
+	}
+
+	@Test
+	void getNumListTest(){
+
+	}
+
+	@Test
+	void getNumListInvalidUUIDTest(){
 
 	}
 }
